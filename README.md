19c695f8d0ccef854160f13eeaf8628334425f4b
DestructionBlack <bitbucket.20.dcaron@spamgourmet.com>
2013-12-01 23:30:54 +0100

    All files_texteditor code is removed, cause we do need it anymore.

--- README.md
+++ README.md
@@ -2,10 +2,13 @@ files_latexeditor
 =================
 
 File Latex Editor/compiler APP for OwnCloud 5.x and above
-You MUST disabled the standard "Text Editor" to allow this one.
+The standard "Text Editor" must be enabled.
 
 change log
 =================
+01.12.2013
+Remove all the files_texteditor code.
+We detect now if the editor is open and then add (or not) the Compile button.
 30.11.2013
 compatibility with OC5
 If a shared directory contains a tex file, friends can compile.
--- l10n/fr.php
+++ l10n/fr.php
@@ -1,13 +1,4 @@
 <?php $TRANSLATIONS = array(
-"Save" => "Enregistrer",
-"Search:" => "Recherche:",
-"Close" => "Fermer",
-"Next" => "Suivant",
-"Clear" => "Effacer",
-"Saving..." => "Enregistrement...",
-"Failed to save file" => "Impossible d'enregistrer le fichier",
-"An error occurred!" => "Une erreur est survenue !",
-"There were unsaved changes, click here to go back" => "Certaines modifications n'ont pas été sauvegardées, cliquez ici pour revenir",
 "COPY ERROR: "	=> 	"Erreur de recopie: ",
 "COPY DONE: "	=> 	"Recopie terminée: ",
 "Compile failed with errors"	=>	"La compilation a échoué avec les erreurs suivantes: "
