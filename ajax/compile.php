54db5839dd0af316a04bd1a97349899cd3cbd5c8
DestructionBlack <bitbucket.20.dcaron@spamgourmet.com>
2013-12-01 11:07:16 +0100

    Commit for OC5 compatibility

--- ajax/compile.php
+++ ajax/compile.php
@@ -3,107 +3,109 @@
 // Check if we are a user
 OCP\JSON::checkLoggedIn();
 
-//$debug=1;
-function my_shell_exec($cmd) {
-    if (isset($debug))
-        return $cmd;
-    return shell_exec($cmd);
-}
+// Get translator
+$l = OC_L10N::get('files_latexeditor');
 
-set_time_limit(300); //scanning can take ages
+set_time_limit(0); //scanning can take ages
 
 $dir = isset($_POST['path']) ? $_POST['path'] : '';
 $pdflatex = isset($_POST['pdflatex']) ? (bool) $_POST['pdflatex'] : false;
 $file = isset($_POST['filename']) ? $_POST['filename'] : '';
 
 $userid = OCP\USER::getUser();
-$filesfoldername = \OCP\Config::getSystemValue('datadirectory') . '/' . $userid . '/files';
-$userDirectory = $filesfoldername;
+// The real directory file
+$workdir = dirname(\OC\Files\Filesystem::getLocalFile(stripslashes($dir). $file));
 $info = pathinfo($file);
 $fileext = '.' . $info['extension'];
 $projectname = trim(basename($file, $fileext));
-$onlyfile = basename($file, $fileext);
-$pdffile = trim(basename($file, $fileext) . '.pdf');
-$dvifile = trim(basename($file, $fileext) . '.dvi');
-$psfile = trim(basename($file, $fileext) . '.ps');
-$logfile = trim(basename($file, $fileext) . '.log');
+$pdffile = $projectname . '.pdf';
+$dvifile = $projectname . '.dvi';
+$psfile = $projectname . '.ps';
+$tocfile = $projectname . '.toc';
+$logfile = $projectname . '.log';
 
-$uid = "latex_$userid" . "_" . $projectname;
-$outpath = "/tmp/$uid";
-$workdir = $userDirectory . $dir;
+// As we will write pdf/ps file(s) in the $dir, we need to known if it's writable
+if(!\OC\Files\Filesystem::isCreatable(stripslashes($dir))) {
+    OCP\JSON::error(array('data' => array('message' => 'As you don\'t have write permission in the owner directory, it\'s not possible to create output latex files.', 'output' => ''))); 
+    exit();
+}
 
-//HEAD
-$command = "mkdir -p  $outpath && cd $workdir && pdflatex -no-shell-escape -output-directory $outpath  $file";
-$output = "\n========BEGIN COMPILE==========\n$command\n";
-$output.=shell_exec(escapeshellarg($command));
-//=======
+$outpath = "/tmp/latex_" . $userid . "_" . $projectname;
+
+$mkdir_command = "mkdir -p  " . $outpath ;
+$cd_command = "cd " . str_replace(' ','\ ',trim($workdir)) ;
 if ($pdflatex === true)
-    $command = "mkdir -p  $outpath && cd $workdir && pdflatex -no-shell-escape -output-directory $outpath  $file";
+    $latex_command .= "pdflatex -output-directory $outpath $file";
 else
-    $command = "mkdir -p  $outpath && cd $workdir && latex -shell-escape -output-directory=$outpath  $file ; cd $outpath; dvips  $dvifile ; ps2pdf $psfile";
-$output = "\n========BEGIN COMPILE==========$psfile\n$command\n";
+    $latex_command .= "latex -output-directory=$outpath  $file ; cd $outpath; dvips  $dvifile ; ps2pdf $psfile";
+$output = "========BEGIN COMPILE $psfile ======== \n "; // % $latex_command\n";
 
-$output.=shell_exec($command);
-//>>>>>>> 3855bba2d28bf11d42b80e9c0e5e70a4f4e692ad
-$output.="\n========END COMPILE==========\n";
+$return = shell_exec($mkdir_command . " && " . $cd_command . " && " . $latex_command);
+$log = file_get_contents($outpath . '/' . $logfile);
 
+while (preg_match('/Rerun to get cross-references right/',$log) || preg_match('/No file '.$tocfile.'/',$log)){
+	$return = shell_exec($cd_command . " && " . $latex_command);
+	$log = file_get_contents($outpath . '/' . $logfile);	
+}
 
+$cleanup = "rm -rf $outpath";
 
-$cleanup = "rm -fr $outpath";
+// ! at begining of a line indicate an error!
+$errors = preg_grep("/^!/",explode("\n",$log)) ;
+if ( empty($errors) === false ) {
+    $log_array = explode("\n",$log);
+    $error = "\n";
+    foreach ( $errors as $line => $msg ) {
+	for ( $i = $line ; $i <= $line + 5 ; $i++)
+		$error .=  $log_array[$i]."\n";
+    }
+    OCP\JSON::error(array('data' => array('message' => $l->t('Compile failed with errors').' - <br/>', 'output' => nl2br($output . " % " . $latex_command . "\n" . $error ))));
+    shell_exec($cleanup);
+    exit;
+}
 
-
-
-$output.= "$outpath/$pdffile\n";
-$output.="$outpath/$logfile\n";
-//$output.="RAW LOG:: $dir/$logfile";
-
-if (!file_exists("$outpath/$pdffile")) {
-    OCP\JSON::error(array('data' => array('message' => 'Compile failed with errors', 'output' => nl2br($output))));
+// No PDF File !?
+if (!file_exists($outpath . '/' . $pdffile)) {
+    OCP\JSON::error(array('data' => array('message' => $l->t('Compile failed with errors').':<br/>', 'output' => nl2br($output . " % ". $latex_command."\n" . file_get_contents($outpath . '/' . $logfile)) )));
+    shell_exec($cleanup);
     exit;
 };
 
-OC_Files::delete($dir, $pdffile);
-OC_Files::delete($dir, $logfile);
-if (!$pdflatex)
-    OC_Files::delete($dir, $psfile);
 
-//$output.="\n>>>> $outpath/$pdffile $workdir$pdffile\n".copy(trim($outpath.'/'.$pdffile), trim($workdir.$pdffile))."\n";
+$output .= $return;
+$output .= "\n========END COMPILE==========\n";
 
-if (!@rename(trim($outpath . '/' . $pdffile), trim($workdir . $pdffile))) {
+if(file_exists($workdir . '/' . $pdffile))
+	\OC\Files\Filesystem::unlink($workdir . '/' . $pdffile);
+if (!$pdflatex && file_exists($workdir . '/' . $psfile) )
+	\OC\Files\Filesystem::unlink($workdir . '/' . $psfile);
+
+if (!@rename(trim($outpath . '/' . $pdffile), trim($workdir . '/'. $pdffile))) {
     $errors = error_get_last();
-    $output.="\n>>>> " . "COPY ERROR: " . $errors['type'];
+    $output.="\n>>>> " . $l->t("COPY ERROR: ") . $errors['type'];
     $output.="\n>>>> " . "<br />\n" . $errors['message'];
-    $output.="<strong>" . trim($outpath . '/' . $pdffile) . " to " . trim($workdir . $pdffile) . "</strong>";
+    $output.="<strong>" . trim($outpath . '/' . $pdffile) . " to " . trim($workdir . '/' . $pdffile) . "</strong>";
 } else {
-    $output.="<strong>" . trim($outpath . '/' . $pdffile) . " to " . trim($workdir . $pdffile) . "</strong>";
+    $output.="<strong> Copy " . trim($outpath . '/' . $pdffile) . " to " . trim($workdir . '/' . $pdffile) . "</strong>";
     if (!$pdflatex) {
-        if (!@rename(trim($outpath . '/' . $psfile), trim($workdir . $psfile))) {
+        if (!@rename(trim($outpath . '/' . $psfile), trim($workdir . '/' . $psfile))) {
             $errors = error_get_last();
-            $output.="\n>>>> " . "COPY ERROR: " . $errors['type'];
+            $output.="\n>>>> " . $l->t("COPY ERROR: ") . $errors['type'];
             $output.="\n>>>> " . "<br />\n" . $errors['message'];
-        } else {
-            
-        }
+        } else
+	    $output.=" <strong> Copy " . trim($outpath . '/' . $psfile) . "</strong>";
     }
 }
 
-$output.="\n>>>> " . "COPY DONE: \n";
-$output.="<strong>" . trim($outpath . '/' . $psfile) . "</strong>";
-//$output.="\n>>>> ";
-//$output.=shell_exec($movepdf);
-//$output.="\n <<<<";
-//$output.="\n" . shell_exec($cleanup);
-//$output.="\n$movepdf\n$cleanup";
-//OC_Files::newFile($dir, $pdffile, 'file');
-$target = OCP\Files::buildNotExistingFileName(stripslashes($dir), $pdffile);
-$meta = OC_FileCache_Cached::get($target);
+$output.="\n>>>> " . $l->t("COPY DONE: ") . "\n";
+$target = OCP\Files::buildNotExistingFileName(stripslashes($workdir), $pdffile);
+$target = \OC\Files\Filesystem::normalizePath($target);
+$meta =  \OC\Files\Filesystem::getFileInfo($target);
 if (!$pdflatex) {
-    $target = OCP\Files::buildNotExistingFileName(stripslashes($dir), $psfile);
-    $meta = OC_FileCache_Cached::get($target);
-}
+    $target = OCP\Files::buildNotExistingFileName(stripslashes($workdir), $psfile);
+    $target = \OC\Files\Filesystem::normalizePath($target);
+    $meta =  \OC\Files\Filesystem::getFileInfo($target);
+} 
 
-
-
-
-//OCP\JSON::success(array('data' => array('output' => nl2br($output), 'mime'=>$meta['mimetype'],'size'=>$meta['size'],'pdffile' => $pdffile, 'logfile' => $logfile)));
 OCP\JSON::success(array('data' => array('output' => nl2br($output), 'path' => $dir, 'pdffile' => $pdffile, 'psfile' => $psfile, 'logfile' => $logfile)));
+shell_exec($cleanup);
--- appinfo/app.php
+++ appinfo/app.php
@@ -1,7 +1,9 @@
 <?php
-//load the required files
-
-OCP\Util::addscript( 'files_latexeditor', 'editor');
-
-
+// only load latex editor if the user is logged in
+if (\OCP\User::isLoggedIn()) {
+        OCP\Util::addStyle('files_latexeditor', 'DroidSansMono/stylesheet'); 
+        OCP\Util::addStyle('files_latexeditor', 'style');  
+        OCP\Util::addscript('files_latexeditor', 'editor');
+        OCP\Util::addscript('files_latexeditor', 'vendor/ace/src-noconflict/ace');
+}
 ?>
