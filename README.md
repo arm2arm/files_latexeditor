8aaa0d5c334db83acfc64141835a994260fa7e7c
Arman Khalatyan <arm2arm@gmail.com>
2014-05-04 12:31:05 +0200

    Version 0.6

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 files_latexeditor
 =================
 
-File Latex Editor/compiler APP for OwnCloud 5.x and above
+File Latex Editor/compiler APP for OwnCloud 6.02 and above
 
 
 change log
 =================
+04.05.2014
+Require Text Editor App 
+Making latexeditor small as possible
+
 30.11.2013
 compatibility with OC5
 If a shared directory contains a tex file, friends can compile.
--- appinfo/app.php
+++ appinfo/app.php
@@ -1,9 +1,10 @@
 <?php
+
 // only load latex editor if the user is logged in
 if (\OCP\User::isLoggedIn()) {
-        OCP\Util::addStyle('files_latexeditor', 'DroidSansMono/stylesheet'); 
-        OCP\Util::addStyle('files_latexeditor', 'style');  
-        OCP\Util::addscript('files_latexeditor', 'editor');
-        OCP\Util::addscript('files_latexeditor', 'vendor/ace/src-noconflict/ace');
+    //OCP\Util::addStyle('files_latexeditor', 'DroidSansMono/stylesheet');
+   // OCP\Util::addStyle('files_latexeditor', 'style');
+    OCP\Util::addscript('files_latexeditor', 'latexutils');
+    OCP\Util::addscript('files_latexeditor', 'latexeditor');
 }
-?>
+
